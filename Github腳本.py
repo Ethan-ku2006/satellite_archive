247da6ca8e21b07d125cc0c8e0fe7103d9982a27
Ethan-ku2006 <kym.ethan0210@gmail.com>
2026-08-24 21:53:22 +0800

    update

--- Github腳本.py
+++ Github腳本.py
@@ -34,6 +34,7 @@ NAME_MAP = {
     "ERS-1&2":"ERS-1&2",
     "WindRAD":"WindRAD",
     "OSCAT":"OSCAT",
+    "SMAP":"SMAP",
     "AMSR":"AMSR",
     "SSMI&SSMIS":"SSMI&SSMIS",
     "TMI&GMI":"TMI&GMI",
